317be56b310878491114a503a23a77330a4b9e9f
Swati-Devi <122820434+Swati-Devi@users.noreply.github.com>
2023-08-26 23:41:04 +0530

    Add files via upload

---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication11
+{
+    public class sample<T>
+    {
+        T[] obj= new T[5];
+        int count = 0;
+
+        public void Add(T item)
+        {
+            if (count<6)
+            {
+                obj[count]=item;
+            }
+            count++;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return obj[index];
+            }
+            set
+            {
+                obj[index]=value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            sample<int> intObj=new sample<int>();
+            intObj.Add(5);
+            intObj.Add(6);
+            intObj.Add(7);
+            intObj.Add(8);
+            intObj.Add(9);
+
+        for(int i=0; i<5; i++)
+        {
+        Console.WriteLine(intObj[i]);
+        }
+        Console.ReadKey();
+        
+    }
+        }
+    }
+
---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication12
+{
+  
+    public class abc<T> { 
+    	private T message; 
+    	public T value 
+    	{ 
+    		get
+    		{ 
+    			return this.message; 
+    		} 
+    		set
+    		{ 
+    			this.message = value; 
+    		} 
+    	} 
+    } 
+
+ class Program
+    {
+       
+       static void Main(string[] args) 
+    	{ 
+    		abc<string> pr = new abc<string>(); 
+    		pr.value = "Hello SWATI";    		
+    		Console.WriteLine("You entered this text::::::::::::"+pr.value);
+            Console.ReadKey();
+
+          }
+   
+        }
+    }
