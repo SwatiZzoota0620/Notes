858c5efb22ac9af27d3cf7d1ff8bcdc26bcf2a7d
Swati-Devi <122820434+Swati-Devi@users.noreply.github.com>
2023-08-27 00:03:52 +0530

    Add files via upload

---
+++ WindowsFormsApplication16/WindowsFormsApplication16/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient; 
+
+namespace WindowsFormsApplication16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=final_practical;Integrated Security=True");            
+                        con.Open(); 
+                        SqlCommand cmd = new SqlCommand();             
+                        cmd.Connection = con;             
+                        cmd.CommandText = "insert into final(name,class)values(@name,@class)";             
+                        cmd.Parameters.AddWithValue("@name",textBox1.Text);             
+                        cmd.Parameters.AddWithValue("@class",textBox2.Text);                       
+                        int i; 
+                        i = cmd.ExecuteNonQuery();             
+                       if (i > 0) 
+                       { 
+                        MessageBox.Show("Data Submitted Successfully!"); 
+                       } 
+ 
+
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void image1_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            DoubleAnimation am = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
+            img.BeginAnimation(Image.OpacityProperty, am);
+
+        }
+
+        private void image1_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            DoubleAnimation am = new DoubleAnimation(1, TimeSpan.FromSeconds(3));
+            img.BeginAnimation(Image.OpacityProperty, am);
+
+        }
+
+       
+
+       
+    }
+}
